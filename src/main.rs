acdecc9be9503f1726f7e9ffbc7e46dfe1793edd
Nora Saulnier <leverplays@gmail.com>
2023-03-03 14:31:40 -0400

    broken sync

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ struct RconPacket {
 	id: i32,
 	ptype: i32,
 	body: String,
-	term: u8,
 }
 
 fn main() {
@@ -28,80 +27,95 @@ fn main() {
 
 	loop {
 		println!("listening for packet");
-		let mut buf = [0; 32];
-
-		stream.read(&mut buf).unwrap();
-
-		let buf = buf.to_vec();
-
-		dbg!(&buf);
-
-		let packet = RconPacket {
-			size: (&buf[0] << &buf[1] << &buf[2] << &buf[3]).into(),
-			id: (&buf[4] << &buf[5] << &buf[6] << &buf[7]).into(),
-			ptype: (&buf[8] << &buf[9] << &buf[10] << &buf[11]).into(),
-			body: {
-				let mut return_val = String::new();
-				for i in 12..(buf.len() - 2) {
-					return_val = return_val + (buf[i] as char).to_string().as_str();
-				}
-				return_val.replace("\0", "")
-			},
-			term: buf[buf.len() - 1],
-		};
-
-		dbg!(&packet);
-
-		let mut buf = [0; 32];
-
-		buf[0] = packet.size >> 0;
-		buf[1] = packet.size >> 1;
-		buf[2] = packet.size >> 2;
-		buf[3] = packet.size >> 3;
+		let mut sized_buf = [0; 4096];
+		stream.read(&mut sized_buf).unwrap();
 		
-		dbg!(buf);
 		
-		match packet.ptype {
+		
+		let received_packet = deserialize_packet(&sized_buf);
+
+		match received_packet.ptype {
 			0 => {
 				println!("rec 0");
 			}
 			2 => {
 				println!("rec 2");
 
-				let response = String::new();
+				let packet = RconPacket {
+					size: 11,
+					id: received_packet.id,
+					ptype: 0,
+					body: "HLSW: Thhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhest".to_string(),
+				};
 
-				response.add("rararara");
-
-				stream
-					.write_all(&[
-						8, 0, 0, 0, /*size*/
-						0, 0, 0, 0, /*id*/
-						0, 0, 0, 0, /*type*/
-						0, 0, 0, 0, 0, 0, 0, 0, /*null terminated string + 8 0 bytes*/
-						0, /*final 0 signifying end of packet*/
-					])
-					.unwrap();
-
+				let response_buf = serialize_packet(&packet);
+				// dbg!(&response_buf);
+				stream.write_all(&response_buf).unwrap();
 				stream.flush().unwrap();
 			}
 			3 => {
 				println!("rec 3");
-				stream
-					.write_all(&[
-						8, 0, 0, 0, /*size*/
-						0, 0, 0, 0, /*id*/
-						2, 0, 0, 0, /*type*/
-						0, 0, 0, 0, 0, 0, 0, 0, /*null terminated string*/
-						0, /*final 0 signifying end of packet*/
-					])
-					.unwrap();
+
+				let packet = RconPacket {
+					size: 8,
+					id: received_packet.id,
+					ptype: 2,
+					body: "\0\0\0\0\0\0\0\0".to_string(),
+				};
+
+				let response_buf = serialize_packet(&packet);
+				stream.write_all(&response_buf).unwrap();
 				stream.flush().unwrap();
 			}
 			_ => panic!("wtf"),
 		}
-
-		// break;
 	}
+}
+
+fn deserialize_packet(buf: &[u8]) -> RconPacket {
+	let deserialized = RconPacket {
+		size: (&buf[0] << &buf[1] << &buf[2] << &buf[3]).into(),
+		id: (&buf[4] << &buf[5] << &buf[6] << &buf[7]).into(),
+		ptype: (&buf[8] << &buf[9] << &buf[10] << &buf[11]).into(),
+		body: {
+			let mut return_val = String::new();
+			for i in 12..(buf.len() - 2) {
+				return_val = return_val + (buf[i] as char).to_string().as_str();
+			}
+			return_val.replace("\0", "")
+		},
+	};
+	dbg!(&deserialized);
+	deserialized
+}
+
+fn serialize_packet(serialized: &RconPacket) -> Vec<u8> {
+	let max_body_size = serialized.body.clone().len() - 1;
+
+	let mut buf = vec![0; max_body_size + 14];
+
+	buf[0] = serialized.size as u8;
+	buf[1] = (serialized.size >> 8) as u8;
+	buf[2] = (serialized.size >> 16) as u8;
+	buf[3] = (serialized.size >> 24) as u8;
+
+	buf[4] = serialized.id as u8;
+	buf[5] = (serialized.id >> 8) as u8;
+	buf[6] = (serialized.id >> 16) as u8;
+	buf[7] = (serialized.id >> 24) as u8;
+
+	buf[8] = serialized.ptype as u8;
+	buf[9] = (serialized.ptype >> 8) as u8;
+	buf[10] = (serialized.ptype >> 16) as u8;
+	buf[11] = (serialized.ptype >> 24) as u8;
+
+	for c in serialized.body.clone().char_indices() {
+		buf[c.0 + 12] = c.1 as u8;
+	}
+
+	dbg!(&serialized);
+
+	buf
 }
 
 fn main2() {
